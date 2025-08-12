3a758bb4b09ce04bf5757f3ec2b82aaa309eb0f7
Dhanishmv <danishahmd212@gmail.com>
2025-08-12 10:22:44 +0530

    Update bin3D.py

--- pct_envs/PctDiscrete0/bin3D.py
+++ pct_envs/PctDiscrete0/bin3D.py
@@ -1,7 +1,6 @@
 from .space import Space
 import numpy as np
 import gym
-from gym import spaces
 from .binCreator import RandomBoxCreator, LoadBoxCreator, BoxCreator
 import torch
 import random
@@ -12,7 +11,7 @@ class PackingDiscrete(gym.Env):
                  container_size=(10, 10, 10),
                  item_set=None, data_name=None, load_test_data=False,
                  internal_node_holder=80, leaf_node_holder=50, next_holder=1, shuffle=False,
-                 LNES='EMS',
+                 LNES = 'EMS',
                  **kwags):
 
         self.internal_node_holder = internal_node_holder
@@ -24,28 +23,26 @@ class PackingDiscrete(gym.Env):
         self.size_minimum = np.min(np.array(item_set))
         self.setting = setting
         self.item_set = item_set
-        self.orientation = 6 if self.setting == 2 else 2
-
+        if self.setting == 2: self.orientation = 6
+        else: self.orientation = 2
+        
+        # The class that maintains the contents of the bin.
         self.space = Space(*self.bin_size, self.size_minimum, self.internal_node_holder)
 
+        # Generator for train/test data
         if not load_test_data:
             assert item_set is not None
             self.box_creator = RandomBoxCreator(item_set)
             assert isinstance(self.box_creator, BoxCreator)
-        else:
+        if load_test_data:
             self.box_creator = LoadBoxCreator(data_name)
 
         self.test = load_test_data
-        self.observation_space = gym.spaces.Box(
-            low=0.0,
-            high=self.space.height,
-            shape=((self.internal_node_holder + self.leaf_node_holder + self.next_holder) * 9,),
-            dtype=np.float32
-        )
-
-        self.action_space = gym.spaces.Discrete(self.leaf_node_holder)
+        self.observation_space = gym.spaces.Box(low=0.0, high=self.space.height,
+                                                shape=((self.internal_node_holder + self.leaf_node_holder + self.next_holder) * 9,))
         self.next_box_vec = np.zeros((self.next_holder, 9))
-        self.LNES = LNES
+
+        self.LNES = LNES  # Leaf Node Expansion Schemes: EMS (recommend), EV, EP, CP, FC
 
     def seed(self, seed=None):
         if seed is not None:
@@ -56,11 +53,10 @@ class PackingDiscrete(gym.Env):
             self.SEED = seed
         return [seed]
 
+    # Calculate space utilization inside a bin.
     def get_box_ratio(self):
         coming_box = self.next_box
-        return (coming_box[0] * coming_box[1] * coming_box[2]) / (
-            self.space.plain_size[0] * self.space.plain_size[1] * self.space.plain_size[2]
-        )
+        return (coming_box[0] * coming_box[1] * coming_box[2]) / (self.space.plain_size[0] * self.space.plain_size[1] * self.space.plain_size[2])
 
     def reset(self):
         self.box_creator.reset()
@@ -70,20 +66,22 @@ class PackingDiscrete(gym.Env):
         cur_observation = self.cur_observation()
         return cur_observation
 
+    # Count and return all PCT nodes.
     def cur_observation(self):
         boxes = []
         leaf_nodes = []
         self.next_box = self.gen_next_box()
 
         if self.test:
-            self.next_den = self.next_box[3] if self.setting == 3 else 1
+            if self.setting == 3: self.next_den = self.next_box[3]
+            else: self.next_den = 1
             self.next_box = [int(self.next_box[0]), int(self.next_box[1]), int(self.next_box[2])]
         else:
-            self.next_den = np.random.random()
-            while self.setting >= 3 and self.next_den == 0:
+            if self.setting < 3: self.next_den = 1
+            else:
                 self.next_den = np.random.random()
-            if self.setting < 3:
-                self.next_den = 1
+                while self.next_den == 0:
+                    self.next_den = np.random.random()
 
         boxes.append(self.space.box_vec)
         leaf_nodes.append(self.get_possible_position())
@@ -94,14 +92,16 @@ class PackingDiscrete(gym.Env):
         self.next_box_vec[:, -1] = 1
         return np.reshape(np.concatenate((*boxes, *leaf_nodes, self.next_box_vec)), (-1))
 
+    # Generate the next item to be placed.
     def gen_next_box(self):
         return self.box_creator.preview(1)[0]
 
+    # Detect potential leaf nodes and check their feasibility.
     def get_possible_position(self):
-        if self.LNES == 'EMS':
-            allPostion = self.space.EMSPoint(self.next_box, self.setting)
+        if   self.LNES == 'EMS':
+            allPostion = self.space.EMSPoint(self.next_box,  self.setting)
         elif self.LNES == 'EV':
-            allPostion = self.space.EventPoint(self.next_box, self.setting)
+            allPostion = self.space.EventPoint(self.next_box,  self.setting)
         elif self.LNES == 'EP':
             allPostion = self.space.ExtremePoint2D(self.next_box, self.setting)
         elif self.LNES == 'CP':
@@ -109,7 +109,7 @@ class PackingDiscrete(gym.Env):
         elif self.LNES == 'FC':
             allPostion = self.space.FullCoord(self.next_box, self.setting)
         else:
-            raise ValueError('Wrong LNES')
+            assert False, 'Wrong LNES'
 
         if self.shuffle:
             np.random.shuffle(allPostion)
@@ -128,17 +128,16 @@ class PackingDiscrete(gym.Env):
                 tmp_list.append([xs, ys, zs, xe, ye, self.bin_size[2], 0, 0, 1])
                 leaf_node_idx += 1
 
-            if leaf_node_idx >= self.leaf_node_holder:
-                break
+            if leaf_node_idx >= self.leaf_node_holder: break
 
-        if tmp_list:
+        if len(tmp_list) != 0:
             leaf_node_vec[0:len(tmp_list)] = np.array(tmp_list)
 
         return leaf_node_vec
 
+    # Convert the selected leaf node to the placement of the current item.
     def LeafNode2Action(self, leaf_node):
-        if np.sum(leaf_node[0:6]) == 0:
-            return (0, 0, 0), self.next_box
+        if np.sum(leaf_node[0:6]) == 0: return (0, 0, 0), self.next_box
         x = int(leaf_node[3] - leaf_node[0])
         y = int(leaf_node[4] - leaf_node[1])
         z = list(self.next_box)
@@ -150,10 +149,8 @@ class PackingDiscrete(gym.Env):
         return action, next_box
 
     def step(self, action):
-        if len(action) != 3:
-            action, next_box = self.LeafNode2Action(action)
-        else:
-            next_box = self.next_box
+        if len(action) != 3: action, next_box = self.LeafNode2Action(action)
+        else: next_box = self.next_box
 
         idx = [action[1], action[2]]
         bin_index = 0
@@ -162,29 +159,30 @@ class PackingDiscrete(gym.Env):
 
         if not succeeded:
             reward = 0.0
-            terminated = True
-            truncated = False
+            done = True
             info = {'counter': len(self.space.boxes), 'ratio': self.space.get_ratio(),
                     'reward': self.space.get_ratio() * 10}
-            return self.cur_observation(), reward, terminated, truncated, info
+            return self.cur_observation(), reward, done, info
 
+        ################################################
+        ############# cal leaf nodes here ##############
+        ################################################
         packed_box = self.space.boxes[-1]
 
-        if self.LNES == 'EMS':
+        if  self.LNES == 'EMS':
             self.space.GENEMS([packed_box.lx, packed_box.ly, packed_box.lz,
-                               packed_box.lx + packed_box.x, packed_box.ly + packed_box.y,
-                               packed_box.lz + packed_box.z])
+                                           packed_box.lx + packed_box.x, packed_box.ly + packed_box.y,
+                                           packed_box.lz + packed_box.z])
 
         self.packed.append(
             [packed_box.x, packed_box.y, packed_box.z, packed_box.lx, packed_box.ly, packed_box.lz, bin_index])
 
         box_ratio = self.get_box_ratio()
-        self.box_creator.drop_box()
-        self.box_creator.generate_box_size()
+        self.box_creator.drop_box()  # remove current box from the list
+        self.box_creator.generate_box_size()  # add a new box to the list
         reward = box_ratio * 10
 
-        terminated = False
-        truncated = False
-        info = {'counter': len(self.space.boxes)}
-
-        return self.cur_observation(), reward, terminated, truncated, info
+        done = False
+        info = dict()
+        info['counter'] = len(self.space.boxes)
+        return self.cur_observation(), reward, done, info
